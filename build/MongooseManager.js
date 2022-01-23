47fa27cf0512e1c59c891c6be20f488276590c12
Jeevan <jeevanppant@gmail.com>
2022-01-24 03:54:23 +0530

    add: initial build

---
+++ build/MongooseManager.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * @name HalukaMongoose
+ * @author Jeevan Prakash Pant
+ */
+class MongooseManager {
+    constructor(ConfigName) {
+        this.ConfigName = ConfigName;
+        // create connection for all configs and return connections
+    }
+}
+exports.default = MongooseManager;
+//# sourceMappingURL=MongooseManager.js.map
---
+++ build/index.d.ts
@@ -0,0 +1,11 @@
+import { IServiceProvider, ServiceProvider } from '@haluka/core';
+/**
+ * @name HalukaMongoose
+ * @author Jeevan Prakash Pant
+ */
+export default class MongooseServiceProvider extends ServiceProvider implements IServiceProvider {
+    /**
+     * Registers Service Provider
+     */
+    register(): void;
+}
