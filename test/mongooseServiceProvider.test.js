4b13c69c691e2068e62bfd87165cef413da9eb0f
Jeevan <jeevanppant@gmail.com>
2022-01-24 18:40:50 +0530

    add: test files

---
+++ test/mongooseServiceProvider.test.js
@@ -0,0 +1,13 @@
+'use strict'
+require('jest')
+
+const { Container } = require('../build/index.js')
+
+describe('Container', () => {
+
+	test('connections from config', () => {
+
+        expect('1').toBe('1')
+
+    })
+})
