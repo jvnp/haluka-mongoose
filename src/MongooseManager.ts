2ac2e2a3dd754c6302139c392773d0b3b27666ec
Jeevan <jeevanppant@gmail.com>
2022-01-27 14:43:54 +0530

    wip: setup for mongoose

--- src/MongooseManager.ts
+++ src/MongooseManager.ts
@@ -1,4 +1,5 @@
 import * as mongoose from 'mongoose'
+import * as _ from 'lodash';
 
 /**
  * @name HalukaMongoose
@@ -6,13 +7,74 @@ import * as mongoose from 'mongoose'
  */
 
 export default class MongooseManager {
-    public ConfigName: string;
-    constructor(ConfigName: string){
-        this.ConfigName = ConfigName
 
-        // create connection for all configs and return connections
+    /**
+      * Configs from configuaration file
+      */
+    protected config: any;
+
+    /**
+     * Check if database is connected
+     */
+    private booted: boolean;
+
+    /**
+     * Array of connections
+     */
+    protected connections: any[];
+
+    constructor(config: any): any {
+        this.config = config;
+        this.booted = false;
     }
 
-    // TODO: function to return connections from config
+    private function createConnection(conf): Promise<any> {
+        var connString = `mongodb://${conf.username}:${conf.password}@${conf.host}:${conf.port}/${conf.database}`;
+        var conn = await mongoose.createConnection(connString, Object.assign({ useNewUrlParser: true }, conf.options));
+        return conn
+    };
+
+    private function setup (): Promise<void> {
+		// Setup All Database
+		for (var conf in this.config.connections) {
+			var connection = this.config.connections[conf]
+			this.connections[conf] = await createConnection(connection)
+			this.app.use('Axe/Events').fire('Database.Connected', conf, connection)
+		}
+		if (!!this.config['default'] && !!this.config['connections'] && this.config.default in this.config['connections']) {
+			this.connections['default'] = this.connections[this.config.default]
+		}
+		this.booted = true;
+	}
+
+	private function booted (): boolean {
+		return this.booted;
+	}
+
+	public function default ():any {
+		return this.connections['default'];
+	}
+
+	function using (conn): any {
+		if (this.connections[conn])
+			return this.connections[conn]
+		else
+			throw new Error(`No database connection exists  with name '${conn}'. Please check your database config.`)
+	}
+
+	async function close (conn): void {
+		if (!!this.connections[conn]) {
+            await (this.connections[conn]).close()
+            // TODO: fire event (if needed)
+
+		}
+	}
+
+	async function closeAll (): void {
+		for (var conn in _.omit(this.connections, ['default'])) {
+            await this.close(conn)
+            // TODO: fire event (if needed)
+		}
+	}
 
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 
 import { Application, IServiceProvider, ServiceProvider } from '@haluka/core'
 
+import MongooseManager from './MongooseManager'
+
 /**
  * @name HalukaMongoose
  * @author Jeevan Prakash Pant
@@ -14,9 +16,9 @@ export default class MongooseServiceProvider extends ServiceProvider implements 
 	public register (): void {
 		this.app.singleton('Mongoose/Manager', function (app: Application) {
 
-			let mongooseConfig = config('mongoose')
-
-			return 0
+			// const mongooseConfig = config('mongoose')
+			const mongooseConfig = ('../test/config/mongoose')
+			return new MongooseManager(mongooseConfig)
 
 		})
 	}
